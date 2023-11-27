6dfdbc58946276a53f32d15e81841ca6e15c96b3
Daniele <iatcodaniele@gmail.com>
2023-11-27 09:05:56 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,14 +6,12 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
     unsigned char *str2 = (char *)s2;
     size_t i = 0;
 
-    if(n == 0)
-        return 0;
-
-    while(str1[i] && str2[i] && i < n)
+    while(i < n && str1[i] == str2[i])
     {
-        str1[i++];
-        str2[i++];
+       i++;
     }
+    if(i = n)
+        return 0;
     return (str1[i] - str2[i]);
 }
 
