8bcb5606f9a402433c072bc162c877615349d1b2
Daniele <iatcodaniele@gmail.com>
2023-12-14 16:51:20 +0100

    Update ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -23,3 +23,11 @@ void	ft_putchar_fd(char c, int fd)
 // 	ft_putchar_fd(c, STDOUT_FILENO);
 // 	return 0;
 // }
+
+//A file descriptor is a number that represents an open file in a computer's operating system.
+//In Unix-like operating systems, everything is treated as a file, and interactions with files are abstracted through file descriptors.
+//File descriptors are used by programs to uniquely identify and refer to open files or input/output resources. 
+//When you open a file or create a network connection, the operating system assigns a file descriptor to that resource.
+//Programs use file descriptors to perform various operations on files, such as reading, writing, and seeking.
+//File descriptors provide a standardized way for programs to interact with different types of resources 
+//without needing to know the specifics of each.
