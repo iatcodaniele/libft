8e62cebd19e5635c14457747c7cd456e6ccc9f5f
Daniele <iatcodaniele@gmail.com>
2023-12-14 17:00:30 +0100

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -24,6 +24,16 @@ void	ft_putstr_fd(char *s, int fd)
 	}
 }
 
+void ft_putstr_fd(char *s, int fd)
+{
+	int i = 0;
+	while(s[i])
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
+}
+
 // int main()
 // {
 // 	char s[] = "hello";
